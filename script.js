5de5ba0d6639f863af0128d4b26f97e08eebf088
Danny Roubin <dannyroubin1@gmail.com>
2021-04-09 12:41:53 -0700

    started the process of getting the random letters onto the generate password slot. Do not run it, it will crash when you press generate

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,13 +17,21 @@ function writePassword() {
 //Here is where I have the java to ask the user all the questions
 //for their password
 var generatePassword = function() {
+  //sets default value to 0
+  var lower = 0;
+  var upper = 0;
+  var special = 0;
 var length = window.prompt("Enter a number between 8 and 128:");
 if (!length) {
   return;
 }
   var lower = window.confirm("Would you like lowercase letters?");
+  //if confirmed, then it changes the value of lower to 1 for later
+  if (confirm (lower = 1))
   var upper = window.confirm("Would you like uppercasecase letters?");
+  if (confirm (upper = 1))
   var special = window.confirm("Would you like special characters?");
+  if (confirm (special = 1))
 
 
 //my bridge from the math functions to displaying the password
@@ -54,11 +63,13 @@ function getRanSpec() {
   return String.fromCharCode(Math.floor(Math.random() * 15) + 32);
   
  }
-//}
- 
-//  console.log(getRanUp());
-//  console.log(getRanLow());
-//  console.log(getRanNum());
-//  console.log(getRanSpec());
- 
+//computes the password here
+var i;
+for (i = 0; i < length;) {
+if (lower == 1) {
+  console.log (lower)
+} 
 }
+}
+
+
