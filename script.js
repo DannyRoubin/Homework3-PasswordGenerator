acded6dbd1264edde582fe9d0ab6854659d9939f
Danny Roubin <dannyroubin1@gmail.com>
2021-04-08 18:38:52 -0700

    cleaned the code up a bit and got alerts popping up. My next step is going to make the user inputs spit out a password

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
 
-// }
-var newPass = document.getElementById('newPass') 
-var length = document.getElementById('length') 
-var upCase = document.getElementById('upCase') 
-var lowCase = document.getElementById('lowCase') 
-var special = document.getElementById('special') 
-var genPassButton = document.getElementById('genPassButton') 
+}
+
+//Here is where I have the java to ask the user all the questions
+//for their password
+var generatePassword = function() {
+var length = window.prompt("Enter a number between 8 and 128:");
+if (!length) {
+  return;
+}
+  var lower = window.confirm("Would you like lowercase letters?");
+  var upper = window.confirm("Would you like uppercasecase letters?");
+  var special = window.confirm("Would you like special characters?");
+
+
+//my bridge from the math functions to displaying the password
+var randomfunc = {
+  lower: getRanLow,
+  upper: getRanUp,
+  Number: getRanNum,
+  special: getRanSpec
+}
 
 
 
+//Here's my math for picking random capital letters/num/symbs using char code
 function getRanUp() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
@@ -36,12 +54,11 @@ function getRanSpec() {
   return String.fromCharCode(Math.floor(Math.random() * 15) + 32);
   
  }
+//}
  
+//  console.log(getRanUp());
+//  console.log(getRanLow());
+//  console.log(getRanNum());
+//  console.log(getRanSpec());
  
- console.log(getRanUp());
- console.log(getRanLow());
- console.log(getRanNum());
- console.log(getRanSpec());
- 
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+}
