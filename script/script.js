af81fb8720c3d3b8ab8ef017f0f3d494a361ee8c
Danny Roubin <dannyroubin1@gmail.com>
2021-04-13 14:10:48 -0700

    Switched up my js completely and made it much more simple, finishing the last few bugs and working on getting the password to print properly

--- script/script.js
+++ script/script.js
@@ -13,6 +13,12 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChars = lowerCaseChars.toUpperCase();
+var numbers = "1234567890";
+var specialChars = "`~!@#$%^&*()_-+=;:/?.,><";
+var characterPool = ""
+var Length;
 
 //Here is where I have the java to ask the user all the questions
 //for their password
@@ -23,56 +29,39 @@ if (length<8) {
   return;
 }
   var lower = window.confirm("Would you like lowercase letters?");
-  var upper = window.confirm("Would you like uppercasecase letters?");
+  if (lower){
+  characterPool += lowerCaseChars
+  }
+  var upper = window.confirm("Would you like uppercase letters?");
+  if (upper){
+    characterPool += upperCaseChars
+    }
   var number = window.confirm("Would you like numbers?");
+  if (number){
+    characterPool += numbers
+    }
   var special = window.confirm("Would you like special characters?");
+  if (special){
+    characterPool += specialChars
+    }
+characterPool.length
 
+     console.log(characterPool);
 
-//my bridge from the math functions to displaying the password
-var randomfunc = {
-  lower: getRanLow,
-  upper: getRanUp,
-  number: getRanNum,
-  special: getRanSpec
+function randomIndex(maxLength) {
+  return Math.floor(Math.random() * maxLength);
+}
+
+var newPassword = "";
+
+for(var i = 0 ; i < Length; i++) {
+ newPassword += characterPool.charAt(randomIndex());
 }
 
 
-
-//Here's my math for picking random capital letters/num/symbs using char code
-function getRanUp() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+ return newPassword;
 }
 
 
-function getRanLow() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-
-function getRanNum() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRanSpec() {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 32);
-  
- }
-//NOTE FOR SELF MAKE AN ARRAY WITH ALL MY VALUES 
-//NO MAKE TWO ONE TO GUARATNEE ONE OF EACH VARIABLE GETS IN AND ANOTHER
-// TO MAKE IT RANDOM 
-
-//first array here checks what is included then will print those out
-var randomLetters = [lower, upper, number, special];
-
-console.log(randomLetters)
-
-
-//computes the password here
- for (var i = 0; i < length; i++) {
-if (lower) {
-  writePassword ;
-} 
-}
-}
 
 
