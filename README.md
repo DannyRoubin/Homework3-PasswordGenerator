9ac1040881b39ef0a48b9d120f1c60d4a8163f2a
Danny Roubin <dannyroubin1@gmail.com>
2021-04-13 22:41:37 -0700

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Homework3-PasswordGenerator
+
+## Description
+The goal of this project was to create a password generator that would produce a secure password for an employee looking to obtain a secure password.
+
+Although this was a tough project, I'm glad I had the chance to complete it because it allowed me to learn the valuable lesson of breaking my code down step by step and carefully analyzing what is being asked from the computer in each line. 
+
+
+## Usage
+To use the website, you simply press the red button that says "Generate Password" on it, then you follow the instructions which are given to you.
+Below you will see a screenshot of what the website looks like after a password is generated
+
+![finished website](assets/images/finishedWebsite.png)
--- script/script.js
+++ script/script.js
@@ -13,54 +13,64 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//Following chunk of vars simply writes out all of the available
+//characters, and leaves empty strings for character pool a
 var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseChars = lowerCaseChars.toUpperCase();
 var numbers = "1234567890";
 var specialChars = "`~!@#$%^&*()_-+=;:/?.,><";
 var characterPool = ""
-var Length;
+var length;
+
+//picks a random character from the character pool
+function randomIndex() { 
+return characterPool.charAt(Math.floor(Math.random() * characterPool.length));
+}
 
 //Here is where I have the java to ask the user all the questions
 //for their password
 var generatePassword = function() {
 
+  //asks user how long they want their password to be
 var length = window.prompt("Enter a number between 8 and 128:");
-if (length<8) {
+if (length < 8) {
   return;
 }
+//asks for lowercase letters
   var lower = window.confirm("Would you like lowercase letters?");
   if (lower){
   characterPool += lowerCaseChars
   }
+  //asks for uppercase letters
   var upper = window.confirm("Would you like uppercase letters?");
   if (upper){
     characterPool += upperCaseChars
     }
+    //asks for numbers
   var number = window.confirm("Would you like numbers?");
   if (number){
     characterPool += numbers
     }
+    //asks for special characters
   var special = window.confirm("Would you like special characters?");
   if (special){
     characterPool += specialChars
     }
-characterPool.length
+  
+//sets new password as an array
+ var newPassword = [];
 
-     console.log(characterPool);
-
-function randomIndex(maxLength) {
-  return Math.floor(Math.random() * maxLength);
+ //pushes random character from the random index the amount of times 
+ //the length is
+for(var i = 0 ; i < length; i++) {
+ newPassword.push(randomIndex());
 }
 
-var newPassword = "";
-
-for(var i = 0 ; i < Length; i++) {
- newPassword += characterPool.charAt(randomIndex());
+//returns the new password from above as a string instead of an array
+return newPassword.join('');
 }
 
 
- return newPassword;
-}
 
 
 
